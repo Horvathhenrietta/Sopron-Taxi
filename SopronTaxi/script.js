6777e7ba4d2c5b9eee00d2b3432f52dfb753f543
Horvathhenrietta <103074457+Horvathhenrietta@users.noreply.github.com>
2022-07-05 11:40:26 +0200

    Update script.js

--- SopronTaxi/script.js
+++ SopronTaxi/script.js
@@ -2,13 +2,18 @@
 
 // MOBILE NAVIGATION
 const navigation = document.querySelector('.main-nav');
-
+const navlinks = document.querySelectorAll('.main-nav-link');
 const opencloseBtns = document.querySelectorAll('.icon-mobile-nav');
 const openMenu = function () {
   navigation.classList.toggle('nav-open');
   opencloseBtns.forEach((btn) => btn.classList.toggle('hidden'));
 };
 opencloseBtns.forEach((btn) => btn.addEventListener('click', openMenu));
+navlinks.forEach((link) =>
+  link.addEventListener('click', function () {
+    navigation.classList.remove('nav-open');
+  })
+);
 
 // Reveal sections
 
